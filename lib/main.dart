6090de76508dd23bdacc93f913c07760fee6ba6d
Felipe Lodes <felipe.lodes@meseems.com.br>
2021-01-07 12:19:12 -0300

    3d commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weatherApp/TestApp.dart';
+import 'package:weatherApp/Test2_App.dart';
 
 void main() {
-  runApp(TestApp(name: 'felipe', age: 23));
+  runApp(Test2_App(name: 'felipe', age: 23));
 }
