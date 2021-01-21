ec47027cfff9674e0985309ee3c2f38a99096ca9
Felipe Lodes <felipe.lodes@meseems.com.br>
2021-01-21 10:24:21 -0300

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+
 import 'package:flutter/material.dart';
-import 'package:weatherApp/infinite_list_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weatherApp/weather/blocs/settings_bloc.dart';
+import 'package:weatherApp/weather/blocs/theme_bloc.dart';
+import 'package:weatherApp/weather/blocs/weather_bloc_observer.dart';
+import 'package:weatherApp/weather/repositories/weather_repository.dart';
+import 'package:weatherApp/weather/weather_app.dart';
+import 'package:http/http.dart' as http;
 
 // void main() {
 //   runApp(MaterialApp(
@@ -12,4 +19,30 @@ import 'package:weatherApp/infinite_list_app.dart';
 //   ));
 // }
 
-void main() => runApp(InfiniteListApp());
+// void main() => runApp(WeatherApp());
+
+void main() {
+  Bloc.observer = WeatherBlocObserver();
+  final WeatherRepository weatherRepository = WeatherRepository(
+      httpClient: http.Client()
+  );
+
+  // final Widget myWidget = MultiBlocProvider(
+  //   providers: [
+  //     BlocProvider<ThemeBloc>(create: (context) => ThemeBloc(),),
+  //     BlocProvider<SettingsBloc>(create: (context) => SettingsBloc(),)
+  //   ],
+  //   child: WeatherApp(weatherRepository: weatherRepository,),
+  // );
+  // runApp(myWidget);
+
+  runApp(
+    BlocProvider<ThemeBloc>(
+      create: (context) => ThemeBloc(),
+      child: BlocProvider<SettingsBloc>(
+        create: (context) => SettingsBloc(),
+        child: WeatherApp(weatherRepository: weatherRepository,),
+      ),
+    )
+  );
+}
